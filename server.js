b754d799720a8b18b8fe6a69a33dc0e32a79ece0
a-oboh <albertoboh7@gmail.com>
2025-01-11 10:54:18 +0100

    init

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+
+
+
